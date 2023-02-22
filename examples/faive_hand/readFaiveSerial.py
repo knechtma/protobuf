d2ec56366f57b8b331f2313cab4fda83e6c384a2
knechtma <knechtma@student.ethz.ch>
2023-02-22 13:48:04 +0100

    adjusted decoding to properly read protocols

--- examples/faive_hand/readFaiveSerial.py
+++ examples/faive_hand/readFaiveSerial.py
@@ -6,16 +6,27 @@ import time
 ser = serial.Serial('/dev/ttyACM0', 9600)
 hand = faive_hand_pb2.Hand()
 
-# instr = '\n:\n\x05thumb\x10\x01\x1a\x17\n\x06HE_MCP\x10\x01"\x0b\x08\x01\x11q=\n\xd7\xa3\xb0(@\x1a\x16\n\x03FSR\x10\x01\x18\x02"\x0b\x08\x01\x11H\xe1z\x14\xae\xf9\x80@'
-
-# hand.ParseFromString(instr)
-
 while True:
-    data = ser.readline()
+    datastr = ser.readline().decode('utf-8')
+
+    data = bytes.fromhex(datastr)
+
     hand.ParseFromString(data)
+
     thumb = hand.fingers[0]
+    for finger in hand.fingers:
+        print("Finger: ", finger.type)
+        for sensor in finger.sensors:
+            if sensor.type:
+                print("   Sensor: ", sensor.type)
+                for data in sensor.data:
+                    if data.value:
+                        print("      Data axs: ", data.axis)
+                        print("      Data val: ", data.value)
+    print(hand.fingers.type)
     print(thumb.type)
     HE1 = thumb.sensors[0]
     print(HE1.type)
     print(HE1.data[0].value)
     time.sleep(1)
+    break
--- examples/faive_hand/sendFaiveSerial.py
+++ examples/faive_hand/sendFaiveSerial.py
@@ -6,17 +6,18 @@ import time
 hand = faive_hand_pb2.Hand()
 
 thumb = hand.fingers.add()
-thumb.id = 1
+# thumb.id = 1
 thumb.name = "thumb"
+thumb.type = faive_hand_pb2.Finger.THUMB
 thumb_HE1 = thumb.sensors.add()
-thumb_HE1.id = 1
+# thumb_HE1.id = 1
 thumb_HE1.name = "HE_MCP"
 thumb_HE1.type = faive_hand_pb2.Sensor.HES
 thumb_HE1_Dat1 = thumb_HE1.data.add()
 thumb_HE1_Dat1.axis = 1
 thumb_HE1_Dat1.value = 12.345
 thumb_FSR = thumb.sensors.add()
-thumb_FSR.id = 1
+# thumb_FSR.id = 1
 thumb_FSR.name = "FSR"
 thumb_FSR.type = faive_hand_pb2.Sensor.FSR
 thumb_FSR_Dat = thumb_FSR.data.add()

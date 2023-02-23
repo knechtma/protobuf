2e51943537d1c7bca89d84c456c14803f5cf15fd
knechtma <knechtma@student.ethz.ch>
2023-02-23 14:48:26 +0100

    Added EventContainers to write and read commands

--- examples/faive_hand/readFaiveSerial.py
+++ examples/faive_hand/readFaiveSerial.py
@@ -5,28 +5,45 @@ import time
 
 ser = serial.Serial('/dev/ttyACM0', 9600)
 hand = faive_hand_pb2.Hand()
+finger = faive_hand_pb2.Finger()
+event = faive_hand_pb2.Event()
 
 while True:
     datastr = ser.readline().decode('utf-8')
 
     data = bytes.fromhex(datastr)
 
-    hand.ParseFromString(data)
+    event.ParseFromString(data)
 
-    thumb = hand.fingers[0]
-    for finger in hand.fingers:
-        print("Finger: ", finger.type)
+    if event.type == 1:
+        print("Reading Hand Data")
+        hand = event.handdata
+        thumb = hand.fingers[0]
+        for finger in hand.fingers:
+            print("Finger: ", finger.type)
+            for sensor in finger.sensors:
+                if sensor.type:
+                    print("   Sensor: ", sensor.type)
+                    for data in sensor.data:
+                        if data.value:
+                            print("      Data axs: ", data.axis)
+                            print("      Data val: ", data.value)
+    elif event.type == 2:
+        print("Reading Finger Data")
+        finger = event.fingerdata
         for sensor in finger.sensors:
-            if sensor.type:
-                print("   Sensor: ", sensor.type)
-                for data in sensor.data:
-                    if data.value:
-                        print("      Data axs: ", data.axis)
-                        print("      Data val: ", data.value)
-    print(hand.fingers.type)
-    print(thumb.type)
-    HE1 = thumb.sensors[0]
-    print(HE1.type)
-    print(HE1.data[0].value)
+                if sensor.type:
+                    print("   Sensor: ", sensor.type)
+                    for data in sensor.data:
+                        if data.value:
+                            print("      Data axs: ", data.axis)
+                            print("      Data val: ", data.value)
+    
+    # print(hand.fingers.type)
+    # print(thumb.type)
+    # HE1 = thumb.sensors[0]
+    # print(HE1.type)
+    # print(HE1.data[0].value)
+    # print(hand)
     time.sleep(1)
     break
